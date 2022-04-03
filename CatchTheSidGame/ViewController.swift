d312010b2a6588853c93a5f5d92d46f03e35b545
mustafacicek06 <mustafa06cck@gmail.com>
2022-04-03 14:07:58 +0300

    Initial Commit

---
+++ CatchTheSidGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatchTheSidGame
+//
+//  Created by Mustafa Çiçek on 3.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
